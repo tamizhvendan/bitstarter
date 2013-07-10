a0bf37e3177535210c44a15e4bf0426082616dd2
Tamizhvendan S <tamizh88@gmail.com>
2013-07-10 10:51:30 +0530

    Fixing formatting errors

--- web.js
+++ web.js
@@ -3,13 +3,11 @@ var express = require('express'),
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-	
+app.get('/', function(request, response) {	
   var data = fs.readFileSync('index.html'),
-	    buffer = new Buffer(data);
-	
-	response.send(buffer.toString("utf8"));
+      buffer = new Buffer(data);
 
+	response.send(buffer.toString("utf8"));
 });
 
 var port = process.env.PORT || 5000;
